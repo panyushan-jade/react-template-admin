51a29a2656544e764ead2966971d773fcce2bd23
panyushan <1193345664@qq.com>
2023-04-13 18:57:29 +0800

    perf: 删除不需要的文件及代码

---
+++ src/global.d.ts
@@ -0,0 +1,21 @@
+declare module "*.less" {
+  const style: any;
+  export default style;
+}
+declare module "*.scss" {
+  const style: any;
+  export default style;
+}
+
+declare module "*.css";
+declare module "*.less";
+declare module "*.sass";
+declare module "*.svg";
+declare module "*.webp";
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.gif";
+declare module "*.bmp";
+declare module "*.tiff";
+declare module "faker";
