64787db63a2a6941dbfa58cafbc756aafc83fb79
panyushan <1193345664@qq.com>
2023-04-23 11:16:39 +0800

    perf: 退出登录

--- src/layout/components/RightContent.tsx
+++ src/layout/components/RightContent.tsx
@@ -1,12 +1,30 @@
 import React from "react";
-import { Avatar } from "antd";
+import { Avatar, Dropdown, MenuProps } from "antd";
 import { UserOutlined } from "@ant-design/icons";
+import { useLoginStore } from "src/stores";
+import { removeLocalStorageItem } from "src/utils/localStorage";
 
 const RightContent: React.FC = () => {
+  const { setUserInfo } = useLoginStore();
+  const logoutHandle = () => {
+    removeLocalStorageItem("userInfo");
+    setUserInfo(null);
+  };
+  const items: MenuProps["items"] = [
+    {
+      key: "1",
+      label: <span onClick={logoutHandle}>退出登录</span>,
+    },
+    {
+      key: "2",
+      label: "个人中心",
+    },
+  ];
+
   return (
-    <>
-      <Avatar icon={<UserOutlined />} />
-    </>
+    <Dropdown menu={{ items }} placement="bottomRight">
+      <Avatar icon={<UserOutlined />} style={{ cursor: "pointer" }} />
+    </Dropdown>
   );
 };
 
